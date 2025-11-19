d67a085621d2a01dae239eeb90915b4189a73bff
Andrej Bajalski <andrejbajalski@gmail.com>
2025-11-19 22:21:22 +0100

    implemented searchbar

--- flutter_test_project/lib/domasna2/screens/meals.dart
+++ flutter_test_project/lib/domasna2/screens/meals.dart
@@ -12,6 +12,9 @@ class MealsPageState extends State<MealsPage> {
   late List<Meal> meals;
   bool _isLoading = true;
   late String categoryName;
+  String searchWord = "";
+  List<Meal> filteredMeals = [];
+  bool filter = false;
 
   @override
   void didChangeDependencies() {
@@ -21,7 +24,7 @@ class MealsPageState extends State<MealsPage> {
         ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
     categoryName = arguments['category'];
     datafetchService.fetchMealsByCategory(categoryName).then((fetchedMeals) {
-      if(mounted){
+      if (mounted) {
         setState(() {
           meals = fetchedMeals;
           _isLoading = false;
@@ -36,15 +39,44 @@ class MealsPageState extends State<MealsPage> {
       appBar: AppBar(title: Text('Meals in $categoryName')),
       body: _isLoading
           ? Center(child: CircularProgressIndicator())
-          : GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 12.0
-              ),
-              itemCount: meals.length,
-              itemBuilder: (context, index) => MealWidget(meal: meals[index]),
-              padding: EdgeInsets.all(22.0)
+          : Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(22.0),
+                  child: SearchBar(
+                    leading: Icon(Icons.search),
+                    hintText: "Search a meal...",
+                    onChanged: (String value) => filterMeals(value)
+                  ),
+                ),
+                Flexible(child:
+                GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 12.0,
+                  ),
+                  itemCount: filter? filteredMeals.length : meals.length,
+                  itemBuilder: (context, index) =>
+                      MealWidget(meal: filter? filteredMeals[index] : meals[index]),
+                  padding: EdgeInsets.all(22.0),
+                )
+                )
+                ,
+              ],
             ),
     );
+  }
+
+  void filterMeals(String searchWord) {
+    setState(() {
+      if (searchWord.isEmpty) {
+        filter = false;
+        return;
+      }
+      filter = true;
+      filteredMeals = meals
+          .where((m) => m.name.toLowerCase().contains(searchWord.toLowerCase()))
+          .toList();
+    });
   }
 }
